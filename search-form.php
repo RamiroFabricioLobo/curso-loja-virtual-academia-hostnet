4c7e3eac977a731a346b6113836b72f9b4a8f665
Ramiro Lobo <37191452+RamiroFabricioLobo@users.noreply.github.com>
2020-10-19 16:12:00 -0300

    Update search-form.php

--- search-form.php
+++ search-form.php
@@ -2,7 +2,7 @@
 // Filtrar buscas somente por produtos
 add_filter( 'pre_get_posts', 'custom_search' );
 function custom_search( $query ) {
-    if ( $query->is_search ) {
+    if ( $query->is_search and !is_admin() ) {
         $query->set( 'post_type', array( 'product' ) );
     }
     return $query;
